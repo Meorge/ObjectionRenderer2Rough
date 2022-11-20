58c10b6e853927d1f2f5b841962235ead3f11d76
Malcolm Anderson <malcolminyo@gmail.com>
2022-11-19 18:29:37 -0800

    Splitting stuff into more files

--- ace_attorney_scene.py
+++ ace_attorney_scene.py
@@ -222,7 +222,6 @@ class ColorOverlayObject(SceneObject):
         if self.remaining > 0:
             ctx.rectangle(xy=(0, 0, img.width, img.height), fill=self.color)
 
-
 class AceAttorneyDirector(Director):
     def __init__(self, fps: float = 30):
         super().__init__(None, fps)
@@ -529,28 +528,3 @@ def get_sprite_location(character: str, emotion: str):
 
 def get_sprite_tag(location: str, character: str, emotion: str):
     return f"<sprite {location} {get_sprite_location(character, emotion)}/>"
-
-B_M = "<startblip male/>"
-B_F = "<startblip female/>"
-B_ST = "<stopblip/>"
-
-SPR_PHX_NORMAL_T = B_M + get_sprite_tag('left', 'phoenix', 'normal-talk')
-SPR_PHX_NORMAL_I = B_ST + get_sprite_tag('left', 'phoenix', 'normal-idle')
-SPR_PHX_SWEAT_T = B_M + get_sprite_tag('left', 'phoenix', 'sweating-talk')
-SPR_PHX_SWEAT_I = B_ST + get_sprite_tag('left', 'phoenix', 'sweating-idle')
-
-SPR_EDW_NORMAL_T = B_M + get_sprite_tag('right', 'edgeworth', 'normal-talk')
-SPR_EDW_NORMAL_I = B_ST + get_sprite_tag('right', 'edgeworth', 'normal-idle')
-
-SLAM_PHX = B_ST + "<deskslam phoenix/><wait 0.8/>"
-SLAM_EDW = B_ST + "<deskslam edgeworth/><wait 0.8/>"
-
-OBJ_PHX = B_ST + "<bubble objection phoenix/><wait 0.8/>"
-OBJ_EDW = B_ST + "<bubble objection edgeworth/><wait 0.8/>"
-HDI_PHX = B_ST + "<bubble holdit phoenix/><wait 0.8/>"
-
-END_BOX = "<showarrow/><wait 2/><hidearrow/><sound pichoop/>"
-
-SHAKE = "<shake 3 0.3/>"
-S_DRAMAPOUND = f"<sound dramapound/><flash 0.15/>{SHAKE}"
-S_SMACK = f"<sound smack/><flash 0.15/>{SHAKE}"
